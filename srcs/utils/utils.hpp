91095cfc25ad4e25acfcf20da17eddf9a4a87c93
GAsNa <romane.leseur.pro@gmail.com>
2022-12-17 20:15:38 +0100

    lexicographical compare - v2

--- srcs/utils/utils.hpp
+++ srcs/utils/utils.hpp
@@ -29,7 +29,10 @@ namespace ft
 
 	/* LEXICOGRAPHICAL_COMPARE */
 	template <class InputIt1, class InputIt2>
-	bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2);
+	bool	lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2);
+
+	template <class InputIt1, class InputIt2, class Compare>
+	bool	lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2, Compare comp);
 
 	//pair
 
